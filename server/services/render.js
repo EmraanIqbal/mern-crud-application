8332151324d7a0b722dfb71b8b40ef5de7753743
MImranIqbal <se.imraniqbal@gmail.com>
2021-12-07 11:03:27 +0500

    server

--- server/services/render.js
+++ server/services/render.js
@@ -1,5 +1,15 @@
+const axios = require("axios");
+
 exports.homeRoute = (req, res) => {
-  res.render("index", {users: "New Data"});
+  axios
+    .get("http://localhost:3000/api/users")
+    .then((response) => {
+      console.log("object::", response.data);
+      res.render("index", { users: response.data });
+    })
+    .catch((error) => {
+      res.send(error);
+    });
 };
 
 exports.addUser = (req, res) => {
